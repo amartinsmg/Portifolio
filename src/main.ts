6819b0bd6c5eec5b51f52d96c7b07b235d8b6add
amartinsmg <andremindurimg@gmail.com>
2022-07-15 20:47:24 -0300

    Addition typing effect

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./sass/main.scss";
+
+function main() {
+  const WhoIAmEl = document.querySelector("#whoiam") as HTMLElement,
+    TextCursor = document.querySelector("#text-cursor") as HTMLElement,
+    WriteName = async () => {
+      const Str = WhoIAmEl.textContent as string;
+      WhoIAmEl.textContent = "";
+      TextCursor.classList.remove("blinker-effect");
+      for (let char of Str) {
+        await new Promise((resolve) => setInterval(resolve, 400));
+        WhoIAmEl.textContent += char;
+      }
+      TextCursor.classList.add("blinker-effect");
+    };
+
+  WriteName();
+}
+
+window.addEventListener("load", main);
