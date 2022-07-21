354831aec449dbd533eb7796795043529c47959d
amartinsmg <andremindurimg@gmail.com>
2022-07-20 23:36:33 -0300

    Addition projects.ejs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import "./sass/main.scss";
 
 async function writeName(): Promise<void> {
-  const WhoIAmEl = document.querySelector("#greeting-content-text-heading-whoiam") as HTMLElement,
-    TextCursor = document.querySelector("#greeting-content-text-heading-text-cursor") as HTMLElement,
+  const WhoIAmEl = document.querySelector(
+      "#greeting-content-text-heading-whoiam"
+    ) as HTMLElement,
+    TextCursor = document.querySelector(
+      "#greeting-content-text-heading-text-cursor"
+    ) as HTMLElement,
     Str = WhoIAmEl.textContent as string;
   WhoIAmEl.textContent = "";
   TextCursor.classList.remove("blinker-effect");
@@ -13,9 +17,31 @@ async function writeName(): Promise<void> {
   TextCursor.classList.add("blinker-effect");
 }
 
+function projectsOver(): void {
+  if (matchMedia("(pointer: fine)")) {
+    const projectDivs = document.querySelectorAll(
+        ".projects-div"
+      ) as NodeListOf<HTMLElement>,
+      projectDivsImgs = document.querySelectorAll(
+        ".projects-div-img"
+      ) as NodeListOf<HTMLElement>;
+    projectDivs.forEach((el) => {
+      el.addEventListener("mouseover", () => {
+        (el.lastElementChild as HTMLElement).style.display = "flex";
+      });
+      el.addEventListener("mouseleave", () => {
+        (el.lastElementChild as HTMLElement).style.display = "none";
+      });
+    });
+    projectDivsImgs.forEach((el) => {
+      el.getClientRects()[0].top;
+    });
+  }
+}
+
 function main() {
   writeName();
-
+  projectsOver();
 }
 
 window.addEventListener("load", main);
